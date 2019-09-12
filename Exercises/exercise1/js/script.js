58c273ab55609531fdb4253bf6ac19b90b685631
Amelle <amelle.margaron@gmail.com>
2019-09-12 17:53:07 -0400

    Ex1: Created moving bird and mouse fixed orange
    also added money png

--- Exercises/exercise1/js/script.js
+++ Exercises/exercise1/js/script.js
@@ -1,61 +1,41 @@
 // Exercise 1 - Movement
-// Pippin Barr
+// Amelle Margaron
 //
-// Starter code for exercise 1.
-// Draws a moving square and circle that intersect
-// in the middle of the canvas.
+// Moves image from left to right side of canvas, constantly displays object at
+// mouse position, and moves one shape in an unexpected manner.
 
-// The current position and size of the circle
-let circleX;
-let circleY;
-let circleSize = 100;
-
-// The current position and size of the square
-let squareX;
-let squareY;
-let squareSize = 100;
-
-//Creating variable for bird pic i'll load later
+//Creating my variables: all my images, some variables for my eventual sine wave
 let birdImage;
 let birdImageX = 0;
 let birdImageY = 10;
 let orangeImage;
 let orangeImageX =0;
 let orangeImageY = 0;
+let a = 0;
+let inc;
+let moneyImage;
 
 // preload()
 //
-// Load images
+// Load images and sine calculation
+
 function preload() {
-  //Linking to cute bird pic that i put in my ex1 files
+  //Linking to bird, orange, money pics that are hosted in my files
   birdImage = loadImage("assets/images/bird.png");
+  orangeImage = loadImage("assets/images/orange.png");
+  moneyImage = loadImage("assets/images/money.png");
+  //
+  let inc = TWO_PI / 25.0;
 }
 
 
 // setup()
 //
-// Set up the canvas, position the images, set the image mode.
+// Set up the canvas
 
 function setup() {
-  // Create our canvas
+  // Create our canvas.
   createCanvas(640,640);
-
-  // Start the circle off screen to the bottom left
-  // We divide the size by two because we're drawing from the center
-  circleX = -circleSize/2;
-  circleY = height + circleSize/2;
-
-  // Start the square off screen to the bottom right
-  // We divide the size by two because we're drawing from the center
-  squareX = width + squareSize/2;
-  squareY = height + squareSize/2;
-
-  // We'll draw rectangles from the center
-  rectMode(CENTER);
-  // We won't have a stroke in this
-  noStroke();
-
-
 
 }
 
@@ -66,26 +46,14 @@ function setup() {
 // Draw the circle and square on screen
 
 function draw() {
-  // We don't fill the background so we get a drawing effect
 
-  // Move circle up and to the right
-  circleX += 1;
-  circleY -= 1;
-  // Make the circle transparent red
-  fill(255,0,0,10);
-  // Display the circle
-  ellipse(circleX,circleY,circleSize,circleSize);
-
-  // Move square up and to the left
-  squareX -= 1;
-  squareY -= 1;
-  // Make the square transparent blue
-  fill(0,0,255,10);
-  // Display the square
-  rect(squareX,squareY,squareSize,squareSize);
-  background(250);
+  background(121,184,220);
   image(birdImage, birdImageX,birdImageY);
   birdImageX += 1;
 
+  image(orangeImage,mouseX,mouseY,60, 50);
 
+
+  image(moneyImage,sin(a), 10);
+    a=a+inc;
 }

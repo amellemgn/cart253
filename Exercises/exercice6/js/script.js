83a5a6a5ec46f0eb0098bb755779f0fa60290bd0
amellemgn <55215308+amellemgn@users.noreply.github.com>
2019-10-29 14:31:23 -0400

    Ex6: Added comments to script.js

--- Exercises/exercice6/js/script.js
+++ Exercises/exercice6/js/script.js
@@ -15,6 +15,7 @@
 let tiger;
 
 // The three prey
+//Fixed: antelop to antelope
 let antelope;
 let zebra;
 let bee;
@@ -23,10 +24,13 @@ let bee;
 //
 // Sets up a canvas
 // Creates objects for the predator and three prey
+//Fixed: added space between function and setup
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  //Fixed: got rid of extra comma in tiger paranthesis
   tiger = new Predator(100, 100, 5, color(200, 200, 0), 40);
   antelope = new Prey(100, 100, 10, color(255, 100, 10), 50);
+  //Fixed: added a y coordinate to zebra
   zebra = new Prey(100, 100, 18, color(255, 255, 255), 60);
   bee = new Prey(100, 100, 20, color(255, 255, 0), 10);
 }
@@ -36,15 +40,18 @@ function setup() {
 // Handles input, movement, eating, and displaying for the system's objects
 function draw() {
   // Clear the background to black
+  //Fixed: spelled background right
   background(0);
 
   // Handle input for the tiger
+  // Fixed: Call tiger.handleInput
   tiger.handleInput();
 
   // Move all the "animals"
   tiger.move();
   antelope.move();
   zebra.move();
+  //Fixed: added bee.move();
   bee.move();
 
   // Handle the tiger eating any of the prey
@@ -54,7 +61,10 @@ function draw() {
 
   // Display all the "animals"
   tiger.display();
+  //Fixed: spell antelope right
   antelope.display();
+  //Fixed: spell display right 
   zebra.display();
+  //Fixed: spell bee right
   bee.display();
 }

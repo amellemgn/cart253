d6c8ca227d28cb1d4201eeb980ba42db91359e28
amellemgn <55215308+amellemgn@users.noreply.github.com>
2019-10-29 13:57:58 -0400

    Ex6: Fixed
    fill the y coordinate my new zebra Prey was missing in script.js setup()

--- Exercises/exercice6/js/script.js
+++ Exercises/exercice6/js/script.js
@@ -27,7 +27,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   tiger = new Predator(100, 100, 5, color(200, 200, 0), 40);
   antelope = new Prey(100, 100, 10, color(255, 100, 10), 50);
-  zebra = new Prey(100, 8, color(255, 255, 255), 60);
+  zebra = new Prey(100, 100, 18, color(255, 255, 255), 60);
   bee = new Prey(100, 100, 20, color(255, 255, 0), 10);
 }
 

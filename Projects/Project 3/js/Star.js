ba8fb4e1b5bcd347f5c406c1467d1347a44e39e2
Amelle <amelle.margaron@gmail.com>
2019-12-06 13:42:03 -0500

    P3: Star class works, clicking on planet objects as star resets array

--- Projects/Project 3/js/Star.js
+++ Projects/Project 3/js/Star.js
@@ -2,7 +2,7 @@
 //
 // star class
 class Star {
-  constructor(x, y /*something else */ ) {
+  constructor(x, y ) {
     this.x = x;
     this.y = y;
   }
@@ -11,7 +11,8 @@ class Star {
   //
   // check where the player is. if it's the correct landstate, display image
   draw() {
-    image(starGif_createImg, this.x, this.y);
+    // image(starGif_createImg0, this.x, this.y);
+    starGif_createImg0.position(this.x, this.y);
   }
   // checkDistance
   //
@@ -19,7 +20,7 @@ class Star {
   //
   checkDistance(planet) {
     this.d = dist(planet.x, planet.y, this.x, this.y);
-    if (this.d < planet.width + this.width) { // planet class has a defined class
+    if (this.d < planet.width + starGif_createImg0.width) { // planet class has a defined class
       planet.playGif = false;
       planet.currentArrayIndex = 0;
     }
